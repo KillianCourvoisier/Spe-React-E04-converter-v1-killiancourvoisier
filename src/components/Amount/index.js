ef0a9072a66997e5abbbcc4eb45d52324c90840d
Killian Courvoisier <dev.killian.courvoisier@gmail.com>
2021-05-05 12:49:18 +0200

    amount static -done-

--- src/components/Amount/index.js
+++ src/components/Amount/index.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 const Amount = () => (
-  <div>Amount</div>
+  <div className="amount">
+    <p className="amount-value">1.09</p>
+    <p className="amount-currency">United States Dollar</p>
+  </div>
 );
 
 export default Amount;
