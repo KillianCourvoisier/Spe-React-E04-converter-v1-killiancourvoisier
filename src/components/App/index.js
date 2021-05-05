5c576dd85d58f7808c7493bb8e297fc5643c7a75
Killian Courvoisier <dev.killian.courvoisier@gmail.com>
2021-05-05 12:02:17 +0200

    Add variables in SCSS

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <div>Header</div>
+    <div>Currencies</div>
+    <div>Amount</div>
+  </div>
+);
+
+// == Export
+export default App;
